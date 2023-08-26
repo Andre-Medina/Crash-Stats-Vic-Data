6d885128b0d5b18f0bf9adc793ef013c99447fb4
Andre-Medina <andremedina@hotmail.com.au>
2023-08-26 11:45:14 +1000

    creating repo structure
    adding main data, folders and documents
    
    ready to create branches

---
+++ production_code/constants.py
@@ -0,0 +1,9 @@
+'''
+Constants used for the rest of the scripts
+'''
+
+
+# data directories from root
+DATA_RAW_DIR = 'data/raw/'
+DATA_PROCESSING_DIR = 'data/processing/'
+DATA_CELAN_DIR = 'data/clean/'
